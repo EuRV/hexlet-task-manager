0efcf0cbdca1b0804ab36c7f26b471b99f1a366a
EuRV <eugenirovensky@yandex.ru>
2025-01-20 15:12:51 +0600

    feat: add route users

--- src/server/core.clj
+++ src/server/core.clj
@@ -2,11 +2,13 @@
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer [defroutes routes]]
   
-            [server.routes.home :refer [home-routes]])
+            [server.routes.home :refer [home-routes]]
+            [server.routes.users :refer [users-routes]])
   (:gen-class))
 
 (defroutes app-routes
-  (routes home-routes))
+  (routes home-routes
+          users-routes))
 
 #_{:clj-kondo/ignore [:unused-binding]}
 (defn -main [& args]
