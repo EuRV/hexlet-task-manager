8b506119ac992d290ea45f91cc72aeb5e731431f
EuRV <eugenirovensky@yandex.ru>
2025-01-22 19:08:01 +0600

    feat: implement on page users output

--- src/server/view/users.clj
+++ src/server/view/users.clj
@@ -1,7 +1,7 @@
 (ns server.view.users)
 
 (defn users
-  []
+  [content]
   [:div.table-responsive
    [:table.table.table-borderless.table-striped.mt-5.bg-white
     [:thead
@@ -11,4 +11,16 @@
       [:th "Email"]
       [:th "Дата создания"]
       [:th "Действия"]]]
-    [:tbody]]])
+    [:tbody
+     (for [[id fname email date] content]
+       [:tr
+        [:td id]
+        [:td fname]
+        [:td email]
+        [:td date]
+        [:td
+         [:div.d-flex.flex-wrap
+          [:a.btn.btn-primary.me-1 {:href (format "users/%s/edit" id)} "Изменить"]
+          [:form {:action (format "users/%s" id) :metod "post"}
+           [:input {:name "_metod" :type "hidden" :value "delete"}]
+           [:input.btn.btn-danger {:type "submit" :value "Удалить"}]]]]])]]])
