c23f1ce60d84c5df938e6df65d41fdd8fdcd90ff
EuRV <eugenirovensky@yandex.ru>
2025-01-24 20:53:17 +0600

    refactor: transfer the table rendering to the mixins

--- src/server/view/users.clj
+++ src/server/view/users.clj
@@ -1,26 +1,15 @@
-(ns server.view.users)
+(ns server.view.users
+  (:require [server.view.mixins :refer [table-render]])
+  (:gen-class))
 
-(defn users
-  [content]
-  [:div.table-responsive
-   [:table.table.table-borderless.table-striped.mt-5.bg-white
-    [:thead
-     [:tr
-      [:th "ID"]
-      [:th "Полное имя"]
-      [:th "Email"]
-      [:th "Дата создания"]
-      [:th "Действия"]]]
-    [:tbody
-     (for [[id fname email date] content]
-       [:tr
-        [:td id]
-        [:td fname]
-        [:td email]
-        [:td date]
-        [:td
-         [:div.d-flex.flex-wrap
-          [:a.btn.btn-primary.me-1 {:href (format "users/%s/edit" id)} "Изменить"]
-          [:form {:action (format "users/%s" id) :metod "post"}
-           [:input {:name "_metod" :type "hidden" :value "delete"}]
-           [:input.btn.btn-danger {:type "submit" :value "Удалить"}]]]]])]]])
+(def data {:entity "users"
+           :headings {:id "ID"
+                      :fname "Полное имя"
+                      :email "Email"
+                      :date "Дата создания"
+                      :action "Действия"}})
+
+(defn users-page [content]
+  [:div.container.wrapper.flex-grow-1
+   [:h1.display-4.fw-bold.mt-4 "Пользователи"]
+   (#(table-render data %) content)])
