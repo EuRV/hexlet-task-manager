a4090e601145b2b2b6d262f85f15f594bb3b75c5
EuRV <eugenirovensky@yandex.ru>
2025-01-24 20:51:10 +0600

    refactor: implement the table rendering

---
+++ src/server/view/mixins.clj
@@ -0,0 +1,28 @@
+(ns server.view.mixins
+  (:gen-class))
+
+(defn table-action [entity id]
+  [:td
+   [:div.d-flex.flex-wrap
+    [:a.btn.btn-primary.me-1 {:href (format "%s/%s/edit" entity id)} "Изменить"]
+    [:form {:action (format "%s/%s" entity id) :metod "post"}
+     [:input {:name "_metod" :type "hidden" :value "delete"}]
+     [:input.btn.btn-danger {:type "submit" :value "Удалить"}]]]])
+
+(defn table-head [headings]
+  (into [] (reduce #(conj % (vector :th %2)) [:tr] (vals headings))))
+
+(defn table-body [entity contents]
+  (for [{:keys [id] :as item} contents
+        :let [new-item (reduce #(conj % (vector :td %2)) [:tr] (vals item))]]
+    (into [] (concat new-item [(table-action entity id)]))))
+
+(defn table-render
+  [data contents]
+  (let [{:keys [entity headings]} data]
+    [:div.table-responsive
+     [:table.table.table-borderless.table-striped.mt-5.bg-white
+      [:thead
+       (table-head headings)]
+      [:tbody
+       (table-body entity contents)]]]))
