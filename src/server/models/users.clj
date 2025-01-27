67b7a2f1d05c6d778cfdb4202f500e00685b8a49
EuRV <eugenirovensky@yandex.ru>
2025-01-27 16:12:15 +0600

    refactor: move formatting functions to helpers

--- src/server/models/users.clj
+++ src/server/models/users.clj
@@ -1,19 +1,7 @@
 (ns server.models.users
-  (:require [server.db :as db])
+  (:require [server.db :as db]
+            [server.helpers :refer [formatter-users]])
   (:gen-class))
-
-(defn formatter-date [date]
-  (.format (java.text.SimpleDateFormat. "MM/dd/yyyy HH:mm:ss") date))
-
-(defn formatter-fname [fname lname]
-  (format "%s %s" fname lname))
-
-(defn formatter-users [user]
-  (let [{:keys [users/id users/first-name users/last-name users/email users/created-at]} user]
-    {:id id
-     :fname (formatter-fname first-name last-name)
-     :email email
-     :date (formatter-date created-at)}))
 
 (defn list-users
   []
