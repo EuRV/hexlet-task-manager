eeb3d5f430bb082ccf766aaa742def6e8f720411
EuRV <eugenirovensky@yandex.ru>
2025-01-27 16:17:59 +0600

    feat: add the User model and its validation

--- src/server/models/users.clj
+++ src/server/models/users.clj
@@ -1,7 +1,27 @@
 (ns server.models.users
-  (:require [server.db :as db]
+  (:require [clojure.spec.alpha :as s]
+
+            [server.db :as db]
             [server.helpers :refer [formatter-users]])
   (:gen-class))
+
+(s/def :user/first-name
+  (s/and string? #(>= (count %) 1)))
+
+(s/def :user/last-name
+  (s/and string? #(>= (count %) 1)))
+
+(s/def :user/email
+  (s/and  string? (partial re-matches #"(.+?)@(.+?)\.(.+?)")))
+
+(s/def :user/password-digest
+  (s/and string? #(>= (count %) 3)))
+
+(s/def :user/person
+  (s/keys :req [:user/first-name
+                :user/last-name
+                :user/email
+                :user/password-digest]))
 
 (defn list-users
   []
