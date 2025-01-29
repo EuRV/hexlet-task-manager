d490249e5127b3a2309e00f77fded41c50943813
EuRV <eugenirovensky@yandex.ru>
2025-01-29 16:56:36 +0600

    feat: add middleware params

--- dev/hotreload.clj
+++ dev/hotreload.clj
@@ -1,12 +1,12 @@
 (ns hotreload
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.reload :refer [wrap-reload]]
-            [server.core :refer [app-routes]])
+            [server.core :refer [app]])
   (:gen-class))
 
 (def dev-handler
-  (wrap-reload #'app-routes))
+  (wrap-reload #'app))
 
 #_{:clj-kondo/ignore [:unused-binding]}
 (defn -main [& args]
-  (run-jetty dev-handler {:port 8080}))
+  (run-jetty #'dev-handler {:port 8080}))
--- src/server/core.clj
+++ src/server/core.clj
@@ -1,15 +1,18 @@
 (ns server.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes routes]]
-  
+            
             [server.routes.home :refer [home-routes]]
             [server.routes.users :refer [users-routes]])
   (:gen-class))
 
 (defroutes app-routes
   (routes home-routes
-          users-routes))
+          users-routes)) 
+
+(def app (wrap-params #'app-routes))
 
 #_{:clj-kondo/ignore [:unused-binding]}
 (defn -main [& args]
-  (run-jetty #'app-routes {:port 3000}))
+  (run-jetty #'app {:port 3000}))
