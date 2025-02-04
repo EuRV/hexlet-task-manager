483c6515efb934aa6f780371969e7b2f85047629
EuRV <eugenirovensky@yandex.ru>
2025-02-04 13:39:48 +0600

    feat: implement error output

--- src/server/view/users.clj
+++ src/server/view/users.clj
@@ -14,7 +14,7 @@
    [:h1.display-4.fw-bold.mt-4 "Пользователи"]
    (#(table-render data %) content)])
 
-(defn users-new []
+(defn users-new [{:keys [errors values]}]
   [:div.container.wrapper.flex-grow-1
    [:h1.display-4.fw-bold.mt-4 "Регистрация"]
    [:div.row.justify-content-center
@@ -27,24 +27,44 @@
           [:input.form-control {:id "data-first-name"
                                 :name "first-name"
                                 :placeholder "Имя"
-                                :type "text"}]
+                                :type "text"
+                                :value (:first-name values)
+                                :class (if (contains? errors :first-name)
+                                         "form-control is-invalid"
+                                         "form-control")}]
+          (when (:first-name errors) [:div.form-control-feedback.invalid-feedback (:first-name errors)])
           [:label {:for "data-first-name"} "Имя"]]
          [:div.form-floating.mb-3
           [:input.form-control {:id "data-last-name"
                                 :name "last-name"
                                 :placeholder "Фамилия"
-                                :type "text"}]
+                                :type "text"
+                                :value (:last-name values)
+                                :class (if (contains? errors :last-name)
+                                         "form-control is-invalid"
+                                         "form-control")}]
+          (when (:last-name errors) [:div.form-control-feedback.invalid-feedback (:last-name errors)])
           [:label {:for "data-last-name"} "Фамилия"]]
          [:div.form-floating.mb-3
           [:input.form-control {:id "data-email"
                                 :name "email"
                                 :placeholder "Email"
-                                :type "text"}]
+                                :type "text"
+                                :value (:email values)
+                                :class (if (contains? errors :email)
+                                         "form-control is-invalid"
+                                         "form-control")}]
+          (when (:email errors) [:div.form-control-feedback.invalid-feedback (:email errors)])
           [:label {:for "data-email"} "Email"]]
          [:div.form-floating.mb-3
           [:input.form-control {:id "data-password"
                                 :name "password-digest"
                                 :placeholder "Пароль"
-                                :type "password"}]
+                                :type "password"
+                                :value (:password-digest values)
+                                :class (if (contains? errors :password-digest)
+                                         "form-control is-invalid"
+                                         "form-control")}]
+          (when (:password-digest errors) [:div.form-control-feedback.invalid-feedback (:password-digest errors)])
           [:label {:for "data-password"} "Пароль"]]
          [:input.btn.btn-primary {:type "submit" :value "Сохранить"}]]]]]]]])
