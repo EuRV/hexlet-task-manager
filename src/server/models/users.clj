22f1d5e5f93a59dcf8148a693b971a8d1d89c475
EuRV <eugenirovensky@yandex.ru>
2025-02-04 13:38:51 +0600

    feat: implement validation and error handling

--- src/server/models/users.clj
+++ src/server/models/users.clj
@@ -6,23 +6,59 @@
             [server.helpers :refer [formatter-users]])
   (:gen-class))
 
-(s/def :user/first-name
-  (s/and string? #(>= (count %) 1)))
+(defn at-least [n]
+  (fn [data]
+    (>= (count data) n)))
 
-(s/def :user/last-name
-  (s/and string? #(>= (count %) 1)))
+(s/def ::string string?)
 
-(s/def :user/email
-  (s/and  string? (partial re-matches #"(.+?)@(.+?)\.(.+?)")))
+(s/def ::at-least-one
+  (at-least 1))
 
-(s/def :user/password-digest
-  (s/and string? #(>= (count %) 3)))
+(s/def ::at-least-three
+  (at-least 3))
 
-(s/def :user/person
-  (s/keys :req [:user/first-name
-                :user/last-name
-                :user/email
-                :user/password-digest]))
+(s/def ::email
+  (partial re-matches #"(.+?)@(.+?)\.(.+?)"))
+
+(s/def :users/first-name
+  (s/and ::string ::at-least-one))
+
+(s/def :users/last-name
+  (s/and ::string ::at-least-one))
+
+(s/def :users/email
+  (s/and ::string ::email))
+
+(s/def :users/password-digest
+  (s/and ::string ::at-least-three))
+
+(s/def :users/person
+  (s/keys :req-un [:users/first-name
+                :users/last-name
+                :users/email
+                :users/password-digest]))
+
+(def spec-errors
+  {::at-least-one "Должно быть не менее одного символа"
+   ::string "Должна быть строкой"
+   ::email "Не соответствует формату email"
+   ::at-least-three "Должно быть не менее трех символов"})
+
+(defn validate-user
+  "Validates a user map and returns a map with :valid? and :errors keys."
+  [user]
+  (if (s/valid? :users/person user)
+    {:valid? true :errors {} :values user}
+    {:valid? false
+     :errors (->> (s/explain-data :users/person user)
+                  :clojure.spec.alpha/problems
+                  (reduce (fn
+                            [init problem]
+                            (assoc init (-> problem :in last) (get spec-errors (-> problem :via peek))))
+                          {}))
+     :values (->> (s/explain-data :users/person user)
+                  :clojure.spec.alpha/value)}))
 
 (defn get-users
   []
@@ -31,3 +67,12 @@
 (defn add-user
   [user]
   (db/insert-data :users user))
+
+(comment
+  (def user {:first-name "Petr"
+             :last-name "Sorokin"
+             :email "sorokin@test.com"
+             :password-digest "asdfg"})
+  
+  (validate-user user)
+  :rcf)
