9161be65f2b6868165789c2dfc340b4be7fcc807
EuRV <eugenirovensky@yandex.ru>
2025-02-04 13:40:37 +0600

    feat: implement error handling

--- src/server/routes/users.clj
+++ src/server/routes/users.clj
@@ -3,14 +3,25 @@
    [compojure.core :refer [defroutes GET POST]]
    [ring.util.response :as resp]
 
-   [server.models.users :refer [get-users add-user]]
+   [server.models.users :refer [get-users add-user validate-user]]
    [server.view.layout :as layout]
    [server.view.users :as view])
   (:gen-class))
 
 (defroutes users-routes
   (GET "/users" {:keys [session]} (layout/common session (view/users-page (get-users))))
-  (GET "/users/new" {:keys [session]} (layout/common session (view/users-new)))
-  (POST "/users" req
-    (add-user (req :params))
-    (resp/redirect "/")))
+  (GET "/users/new" {:keys [session]} (layout/common session (view/users-new {:errors {} :values {}})))
+  (POST "/users" request
+    (let [data (:params request)
+          vval (validate-user data)]
+      (if (:valid? vval)
+        (try
+          (add-user (:values vval))
+          (resp/redirect "/")
+          (catch Exception e
+            (layout/common
+             (:session request)
+             (view/users-new (assoc-in vval [:errors :email] "Такой email уже существует")))))
+        (layout/common
+         (:session request)
+         (view/users-new vval))))))
