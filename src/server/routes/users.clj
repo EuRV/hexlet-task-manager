19342a554d2e2935f1dfe172634d504edbb8081f
EuRV <eugenirovensky@yandex.ru>
2025-02-05 15:34:25 +0600

    feat: implement user deletion handler

--- src/server/routes/users.clj
+++ src/server/routes/users.clj
@@ -1,16 +1,32 @@
 (ns server.routes.users
   (:require
-   [compojure.core :refer [defroutes GET POST]]
+   [compojure.core :refer [defroutes GET POST PATCH DELETE]]
    [ring.util.response :as resp]
 
-   [server.models.users :refer [get-users add-user validate-user]]
+   [server.models.users :refer [get-users add-user validate-user delete-user]]
    [server.view.layout :as layout]
-   [server.view.users :as view])
+   [server.view.users :as view]
+   [server.helpers :refer [to-number]])
   (:gen-class))
+
+(defn delete-user-handler
+  [request]
+  (let [user-id (-> request :params :id to-number)
+        session-user-id (-> request :session :user-id to-number)]
+    (cond
+      (nil? session-user-id) (println session-user-id)
+      (not= user-id session-user-id) (println (type user-id) (type session-user-id))
+      :else (do
+              (delete-user user-id)
+              (let [session (:session request)
+                    updated-session (dissoc session :user-id :email)]
+                (-> (resp/redirect "/users")
+                    (assoc :session updated-session)))))))
 
 (defroutes users-routes
   (GET "/users" {:keys [session]} (layout/common session (view/users-page (get-users))))
   (GET "/users/new" {:keys [session]} (layout/common session (view/users-new {:errors {} :values {}})))
+  (GET "/users/:id/edit" request ())
   (POST "/users" request
     (let [data (:params request)
           vval (validate-user data)]
@@ -24,4 +40,6 @@
              (view/users-new (assoc-in vval [:errors :email] "Такой email уже существует")))))
         (layout/common
          (:session request)
-         (view/users-new vval))))))
+         (view/users-new vval)))))
+  (PATCH "/users/:id" request ())
+  (DELETE "/users/:id" request (delete-user-handler request)))
