44629400b70b8658ae21b5d6331631b4410778fb
EuRV <eugenirovensky@yandex.ru>
2025-02-06 10:34:12 +0600

    feat: implement user edit form

--- src/server/view/users.clj
+++ src/server/view/users.clj
@@ -1,5 +1,7 @@
 (ns server.view.users
-  (:require [server.view.mixins :refer [table-render]])
+  (:require
+   [hiccup.form :as form]
+   [server.view.mixins :refer [table-render]])
   (:gen-class))
 
 (def data {:entity "users"
@@ -68,3 +70,50 @@
           (when (:password-digest errors) [:div.form-control-feedback.invalid-feedback (:password-digest errors)])
           [:label {:for "data-password"} "Пароль"]]
          [:input.btn.btn-primary {:type "submit" :value "Сохранить"}]]]]]]]])
+
+(defn users-edit [{:keys [errors values]}]
+  [:div.container.wrapper.flex-grow-1
+   [:h1.display-4.fw-bold.mt-4 "Изменение пользователя"]
+   (form/form-to [:post (format "/users/%s" (:id values))]
+                 [:div.form-floating.mb-3
+                  [:input#data-first-name.form-control {:name "first-name"
+                                                        :placeholder "Имя"
+                                                        :type "text"
+                                                        :value (:first-name values)
+                                                        :class (if (contains? errors :first-name)
+                                                                 "form-control is-invalid"
+                                                                 "form-control")}]
+                  (when (:first-name errors) [:div.form-control-feedback.invalid-feedback (:first-name errors)])
+                  (form/label {:for "data-first-name"} :first-name "Имя")]
+                 [:div.form-floating.mb-3
+                  [:input#data-last-name.form-control {:name "last-name"
+                                                       :placeholder "Фамилия"
+                                                       :type "text"
+                                                       :value (:last-name values)
+                                                       :class (if (contains? errors :last-name)
+                                                                "form-control is-invalid"
+                                                                "form-control")}]
+                  (when (:last-name errors) [:div.form-control-feedback.invalid-feedback (:last-name errors)])
+                  [:label {:for "data-last-name"} "Фамилия"]]
+                 [:div.form-floating.mb-3
+                  [:input#data-email.form-control {:name "email"
+                                                   :placeholder "Email"
+                                                   :type "text"
+                                                   :value (:email values)
+                                                   :class (if (contains? errors :email)
+                                                            "form-control is-invalid"
+                                                            "form-control")}]
+                  (when (:email errors) [:div.form-control-feedback.invalid-feedback (:email errors)])
+                  [:label {:for "data-email"} "Email"]]
+                 [:div.form-floating.mb-3
+                  [:input#data-password.form-control {:name "password-digest"
+                                                      :placeholder "Пароль"
+                                                      :type "password"
+                                                      :value (:password-digest values)
+                                                      :class (if (contains? errors :password-digest)
+                                                               "form-control is-invalid"
+                                                               "form-control")}]
+                  (when (:password-digest errors) [:div.form-control-feedback.invalid-feedback (:password-digest errors)])
+                  [:label {:for "data-password"} "Пароль"]]
+                 (form/hidden-field :_method :PATCH)
+                 (form/submit-button {:class "btn btn-primary"} "Изменить"))])
