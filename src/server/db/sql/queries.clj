a8f52da7c6651375dbdcb6774aac9cf3ddf89350
EuRV <eugenirovensky@yandex.ru>
2025-02-12 17:07:32 +0600

    refactor: move all requests from core

--- src/server/db/sql/queries.clj
+++ src/server/db/sql/queries.clj
@@ -1,14 +1,50 @@
-(ns server.db.sql.queries)
+(ns server.db.sql.queries
+  (:require
+   [next.jdbc :as jdbc]
+   [next.jdbc.sql :as sql]
+   [next.jdbc.result-set :as rs]
+   
+   [server.db.core :refer [ds]])
+  (:gen-class))
 
-(def queries
-  {:list-users "SELECT * FROM users"})
+(defn query-database
+  [sql-statement]
+  (with-open [connection (jdbc/get-connection ds)]
+    (sql/query connection [sql-statement] jdbc/snake-kebab-opts)))
 
-"CREATE TABLE users (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password_digest VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);"
+(defn query-by-key
+  ([table data] (query-by-key table data nil))
+  ([table data opts]
+   (with-open [connection (jdbc/get-connection ds)]
+     (sql/find-by-keys
+      connection
+      table
+      data
+      (merge {:column-fn (:column-fn jdbc/snake-kebab-opts)
+              :builder-fn rs/as-unqualified-kebab-maps} opts)))))
+
+(defn query-by-id
+  ([table id] (query-by-id table id nil))
+  ([table id opts]
+   (with-open [connection (jdbc/get-connection ds)]
+     (sql/get-by-id
+      connection
+      table
+      id
+      (merge {:column-fn (:column-fn jdbc/snake-kebab-opts)
+              :builder-fn rs/as-unqualified-kebab-maps} opts)))))
+
+(defn insert-data
+  [table record-data]
+  (with-open [connection (jdbc/get-connection ds)]
+    (sql/insert! connection table record-data jdbc/unqualified-snake-kebab-opts)))
+
+(defn update-data
+  [table record-data where-params]
+  (with-open [connection (jdbc/get-connection ds)]
+    (sql/update! connection table record-data where-params jdbc/unqualified-snake-kebab-opts)))
+
+(defn delete-by-key
+  [table key value]
+  (with-open [connection (jdbc/get-connection ds)]
+    (sql/delete! connection table {key value})))
