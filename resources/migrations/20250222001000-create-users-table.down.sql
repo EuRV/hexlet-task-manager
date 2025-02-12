812f20d7cb1be0c9825f990bbb6a9a222c235dfd
EuRV <eugenirovensky@yandex.ru>
2025-02-12 17:09:38 +0600

    fix: add separator

--- resources/migrations/20250222001000-create-users-table.down.sql
+++ resources/migrations/20250222001000-create-users-table.down.sql
@@ -1,4 +1,5 @@
 DROP TRIGGER IF EXISTS users_updated_at_trigger ON users;
+--;;
 DROP FUNCTION IF EXISTS update_users_updated_at;
 --;;
 DROP TABLE users;
--- resources/migrations/20250222002000-create-statuses-table.down.sql
+++ resources/migrations/20250222002000-create-statuses-table.down.sql
@@ -1,4 +1,5 @@
 DROP TRIGGER IF EXISTS statuses_updated_at_trigger ON statuses;
+--;;
 DROP FUNCTION IF EXISTS update_statuses_updated_at;
 --;;
 DROP TABLE statuses;
