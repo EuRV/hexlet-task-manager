60d1be1debc63405abc235cfbbbb7de12b26ed8f
EuRV <eugenirovensky@yandex.ru>
2025-02-13 19:00:50 +0600

    refactor: change the function body arguments

--- src/server/routes/statuses.clj
+++ src/server/routes/statuses.clj
@@ -3,9 +3,8 @@
    [compojure.core :refer [defroutes GET]]
    
    [server.models.statuses :as models]
-   [server.view.layout :as layout]
    [server.view.statuses :as view])
   (:gen-class))
 
 (defroutes statuses-routes
-  (GET "/statuses" request (layout/common request :statuses (view/statuses-page (models/get-statuses)))))
+  (GET "/statuses" request (view/statuses-page request (models/get-statuses))))
