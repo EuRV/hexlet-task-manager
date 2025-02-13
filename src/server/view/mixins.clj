076c5c7866c429e02ce8bdb331fce1d2e671a8e6
EuRV <eugenirovensky@yandex.ru>
2025-02-13 21:31:35 +0600

    refactor: make headers formation with i18n

--- src/server/view/mixins.clj
+++ src/server/view/mixins.clj
@@ -10,8 +10,10 @@
                   [:input {:type "hidden" :name "_method" :value "DELETE"}]
                   [:input.btn.btn-danger {:type "submit" :value "Удалить"}])]])
 
-(defn table-head [headings]
-  (into [] (reduce #(conj % (vector :th %2)) [:tr] (vals headings))))
+(defn table-head [keys i18n]
+  (into [] (reduce #(conj % (vector :th %2)) [:tr] (for [key keys
+                                                         :let [val (key i18n)]]
+                                                     val))))
 
 (defn table-body [entity contents]
   (for [{:keys [id] :as item} contents
@@ -19,10 +21,12 @@
     (into [] (concat new-item [(table-action entity id)]))))
 
 (defn table-render
-  [{:keys [entity headings]} contents]
+  [data i18n contents]
+  (let [entity (-> data keys first name)
+        headings (-> data vals first)]
     [:div.table-responsive
      [:table.table.table-borderless.table-striped.mt-5.bg-white
       [:thead
-       (table-head headings)]
+       (table-head headings i18n)]
       [:tbody
-       (table-body entity contents)]]])
+       (table-body entity contents)]]]))
