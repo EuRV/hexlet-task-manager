15382dc63858b3e2dc5b318c4e29d43f4a40f626
EuRV <eugenirovensky@yandex.ru>
2025-02-13 19:00:11 +0600

    refactor: change the function body arguments

--- src/server/routes/home.clj
+++ src/server/routes/home.clj
@@ -1,9 +1,8 @@
 (ns server.routes.home
   (:require [compojure.core :refer [GET defroutes]]
-
-            [server.view.layout :as layout]
+            
             [server.view.home :as view])
   (:gen-class))
 
 (defroutes home-routes
-  (GET "/" request (layout/common request nil (view/home))))
+  (GET "/" request (view/home request)))
