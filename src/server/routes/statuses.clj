9b4875b51a9f5f404aba128c5733e7cd977d2a2d
EuRV <eugenirovensky@yandex.ru>
2025-02-14 19:34:45 +0600

    feat: create a route and handler statuses-new

--- src/server/routes/statuses.clj
+++ src/server/routes/statuses.clj
@@ -1,10 +1,19 @@
 (ns server.routes.statuses
   (:require
    [compojure.core :refer [defroutes GET]]
-   
+
    [server.models.statuses :as models]
    [server.view.statuses :as view])
   (:gen-class))
 
+(defn statuses-handler
+  [request content]
+  (view/statuses-page request content))
+
+(defn statuses-new-handler
+  [request]
+  (view/statuses-new request))
+
 (defroutes statuses-routes
-  (GET "/statuses" request (view/statuses-page request (models/get-statuses))))
+  (GET "/statuses" request (statuses-handler request (models/get-statuses)))
+  (GET "/statuses/new" request (statuses-new-handler request)))
