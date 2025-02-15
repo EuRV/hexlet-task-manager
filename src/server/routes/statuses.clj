06ab16d7620c2695617c3297e3cac6e604c6df2a
EuRV <eugenirovensky@yandex.ru>
2025-02-15 21:19:38 +0600

    feat: add database error handling

--- src/server/routes/statuses.clj
+++ src/server/routes/statuses.clj
@@ -1,19 +1,30 @@
 (ns server.routes.statuses
   (:require
-   [compojure.core :refer [defroutes GET]]
+   [compojure.core :refer [defroutes GET POST]]
+   [ring.util.response :as resp]
 
    [server.models.statuses :as models]
    [server.view.statuses :as view])
   (:gen-class))
 
 (defn statuses-handler
-  [request content]
-  (view/statuses-page request content))
+  [request]
+  (if (seq (:session request))
+    (let [content (models/get-statuses)]
+     (if (seq (:error content))
+       (->
+        (assoc request :flash {:type "danger" :message (-> content :error :message)})
+        (view/statuses-page (-> content :error :value)))
+       (view/statuses-page request content)))
+    (resp/redirect "/")))
 
 (defn statuses-new-handler
-  [request]
-  (view/statuses-new request))
+  ([request] (statuses-new-handler request {:errors {} :values {}}))
+  ([request data]
+   (if (seq (:session request))
+     (view/statuses-new request data)
+     (resp/redirect "/"))))
 
 (defroutes statuses-routes
-  (GET "/statuses" request (statuses-handler request (models/get-statuses)))
+  (GET "/statuses" request (statuses-handler request))
   (GET "/statuses/new" request (statuses-new-handler request)))
