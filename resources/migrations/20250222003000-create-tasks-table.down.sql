9d4b45792c82801ce7a5fa6fea2f51e78da3bd7d
EuRV <eugenirovensky@yandex.ru>
2025-02-19 15:00:35 +0600

    ops: create migrations for task table

---
+++ resources/migrations/20250222003000-create-tasks-table.down.sql
@@ -0,0 +1,5 @@
+DROP TRIGGER IF EXISTS tasks_updated_at_trigger ON tasks;
+--;;
+DROP FUNCTION IF EXISTS update_tasks_updated_at;
+--;;
+DROP TABLE tasks;
---
+++ resources/migrations/20250222003000-create-tasks-table.up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    description text,
+    status_id INTEGER NOT NULL REFERENCES statuses,
+    creator_id INTEGER NOT NULL REFERENCES users,
+    executor_id INTEGER REFERENCES users,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+--;;
+CREATE OR REPLACE FUNCTION update_tasks_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+--;;
+CREATE TRIGGER tasks_updated_at_trigger
+BEFORE UPDATE ON tasks
+FOR EACH ROW
+EXECUTE FUNCTION update_tasks_updated_at();
