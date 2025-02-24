b7166df9a446bd107ac2c6948ece79b47036788c
EuRV <eugenirovensky@yandex.ru>
2025-02-24 19:03:17 +0600

    refactor: remove user formatter

--- src/server/helpers.clj
+++ src/server/helpers.clj
@@ -1,18 +1,5 @@
 (ns server.helpers)
 
-(defn formatter-date [date]
-  (.format (java.text.SimpleDateFormat. "MM/dd/yyyy HH:mm:ss") date))
-
-(defn formatter-fname [fname lname]
-  (format "%s %s" fname lname))
-
-(defn formatter-users [user]
-  (let [{:keys [id first-name last-name email created-at]} user]
-    {:id id
-     :fname (formatter-fname first-name last-name)
-     :email email
-     :date (formatter-date created-at)}))
-
 (defn to-number [value]
   (cond
     (number? value) value
