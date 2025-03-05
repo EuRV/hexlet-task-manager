e3605473502e0152d0825cf5dd7f01df3e125e70
EuRV <eugenirovensky@yandex.ru>
2025-03-05 14:55:50 +0600

    feat: implement a label validator

--- src/server/models/labels.clj
+++ src/server/models/labels.clj
@@ -1,7 +1,43 @@
 (ns server.models.labels
   (:require
+   [clojure.spec.alpha :as s]
+
    [server.db.sql.queries :as db])
   (:gen-class))
+
+(defn at-least [n]
+  (fn [data]
+    (>= (count data) n)))
+
+(s/def ::at-least-one
+  (at-least 1))
+
+(s/def ::string string?)
+
+(s/def :labels/name
+  (s/and ::string ::at-least-one))
+
+(s/def :labels/entity
+  (s/keys :req-un [:labels/name]))
+
+(def spec-errors
+  {::at-least-one "Должно быть не менее одного символа"
+   ::string "Должна быть строкой"})
+
+(defn validate-label
+  "Validates a labels map and returns a map with :valid? and :errors keys."
+  [labels]
+  (if (s/valid? :labels/entity labels)
+    {:valid? true :errors {} :values labels}
+    {:valid? false
+     :errors (->> (s/explain-data :labels/entity labels)
+                  :clojure.spec.alpha/problems
+                  (reduce (fn
+                            [init problem]
+                            (assoc init (-> problem :in last) (get spec-errors (-> problem :via peek))))
+                          {}))
+     :values (->> (s/explain-data :labels/entity labels)
+                  :clojure.spec.alpha/value)}))
 
 (defn get-labels
   []
