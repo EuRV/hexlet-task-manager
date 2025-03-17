f5a553297a04151001cbf7b0a64fae1c88409a42
EuRV <eugenirovensky@yandex.ru>
2025-03-17 15:47:31 +0600

    refactor: add default value to labels-page

--- src/server/view/labels.clj
+++ src/server/view/labels.clj
@@ -11,16 +11,16 @@
 (def data {:labels [:id :name :date :action]})
 
 (defn labels-page
-  [request content]
-  (layout/common
-   request
-   (let [i18n (get-in request [:translations :tables])]
-     (html
-      [:h1.display-4.fw-bold.mt-4 (get-in request [:translations :layout :labels] "Default")]
-      (link-to {:class "btn btn-primary mb-5"}
-               (format "/%s/new" (-> data keys first name))
-               (get-in request [:translations :labels :new] "Default"))
-      (table-render data i18n content)))))
+  ([request] (labels-page request []))
+  ([request content] (layout/common
+                     request
+                     (let [i18n (get-in request [:translations :tables])]
+                       (html
+                        [:h1.display-4.fw-bold.mt-4 (get-in request [:translations :layout :labels] "Default")]
+                        (link-to {:class "btn btn-primary mb-5"}
+                                 (format "/%s/new" (-> data keys first name))
+                                 (get-in request [:translations :labels :new] "Default"))
+                        (table-render data i18n content))))))
 
 (defn labels-new-page
   [request {:keys [errors values]}]
