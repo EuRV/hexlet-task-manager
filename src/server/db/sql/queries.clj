77c270295eb4cef51e273a837e1315b854072bc0
EuRV <eugenirovensky@yandex.ru>
2025-03-17 15:40:58 +0600

    refactor: add error handling

--- src/server/db/sql/queries.clj
+++ src/server/db/sql/queries.clj
@@ -3,7 +3,7 @@
    [next.jdbc :as jdbc]
    [next.jdbc.sql :as sql]
    [next.jdbc.result-set :as rs]
-   
+
    [server.db.core :refer [ds]])
   (:gen-class))
 
@@ -16,46 +16,59 @@
        sql-statement
        {:builder-fn rs/as-unqualified-kebab-maps}))
     (catch Exception e
-        {:error
-         {:message (ex-message e)
-          :value []}})))
+      (throw e))))
 
 (defn query-by-key
   ([table data] (query-by-key table data nil))
   ([table data opts]
-   (with-open [connection (jdbc/get-connection ds)]
-     (sql/find-by-keys
-      connection
-      table
-      data
-      (merge {:column-fn (:column-fn jdbc/snake-kebab-opts)
-              :builder-fn rs/as-unqualified-kebab-maps} opts)))))
+   (try
+     (with-open [connection (jdbc/get-connection ds)]
+       (sql/find-by-keys
+        connection
+        table
+        data
+        (merge {:column-fn (:column-fn jdbc/snake-kebab-opts)
+                :builder-fn rs/as-unqualified-kebab-maps} opts)))
+     (catch Exception e
+       (throw e)))))
 
 (defn query-by-id
   ([table id] (query-by-id table id nil))
   ([table id opts]
-   (with-open [connection (jdbc/get-connection ds)]
-     (sql/get-by-id
-      connection
-      table
-      id
-      (merge {:column-fn (:column-fn jdbc/snake-kebab-opts)
-              :builder-fn rs/as-unqualified-kebab-maps} opts)))))
+   (try
+     (with-open [connection (jdbc/get-connection ds)]
+       (sql/get-by-id
+        connection
+        table
+        id
+        (merge {:column-fn (:column-fn jdbc/snake-kebab-opts)
+                :builder-fn rs/as-unqualified-kebab-maps} opts)))
+     (catch Exception e
+       (throw e)))))
 
 (defn insert-data
   [table record-data]
-  (with-open [connection (jdbc/get-connection ds)]
-    (sql/insert! connection table record-data jdbc/unqualified-snake-kebab-opts)))
+  (try
+    (with-open [connection (jdbc/get-connection ds)]
+      (sql/insert! connection table record-data jdbc/unqualified-snake-kebab-opts))
+    (catch Exception e
+      (throw e))))
 
 (defn update-data
   [table record-data where-params]
-  (with-open [connection (jdbc/get-connection ds)]
-    (sql/update! connection table record-data where-params jdbc/unqualified-snake-kebab-opts)))
+  (try
+    (with-open [connection (jdbc/get-connection ds)]
+      (sql/update! connection table record-data where-params jdbc/unqualified-snake-kebab-opts))
+    (catch Exception e
+      (throw e))))
 
 (defn delete-by-key
   [table key value]
-  (with-open [connection (jdbc/get-connection ds)]
-    (sql/delete! connection table {key value})))
+  (try
+    (with-open [connection (jdbc/get-connection ds)]
+      (sql/delete! connection table {key value}))
+    (catch Exception e
+      (throw e))))
 
 (defn create-task-with-labels [task-data label-ids]
   (jdbc/with-transaction [tx ds]
