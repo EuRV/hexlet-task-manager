d35d2fbe5c7ffd013012bb9de569d3b8f70dc0e7
EuRV <eugenirovensky@yandex.ru>
2025-04-08 19:55:12 +0600

    refactor: split into public and protected routes

--- src/server/core.clj
+++ src/server/core.clj
@@ -4,11 +4,11 @@
             [ring.middleware.cookies :refer [wrap-cookies]]
             [ring.middleware.session :refer [wrap-session]]
             [compojure.core :refer [defroutes routes]]
-            
-            [server.middleware :refer [wrap-i18n]]
+
+            [server.middleware :refer [wrap-protected wrap-i18n]]
             [server.routes.home :refer [home-routes]]
-            [server.routes.users :refer [users-routes]]
-            [server.routes.session :refer [session-routes]]
+            [server.routes.users :refer [public-users-routes protected-users-routes]]
+            [server.routes.session :refer [public-session-routes protected-session-routes]]
             [server.routes.statuses :refer [statuses-routes]]
             [server.routes.tasks :refer [tasks-routes]]
             [server.routes.labels :refer [labels-routes]])
@@ -16,16 +16,19 @@
 
 (defroutes app-routes
   (routes home-routes
-          users-routes
-          session-routes
-          statuses-routes
-          tasks-routes
-          labels-routes))
+          public-session-routes
+          public-users-routes
+          (wrap-protected
+           (routes protected-session-routes
+                   protected-users-routes
+                   statuses-routes
+                   tasks-routes
+                   labels-routes))))
 
 (def app
   (-> #'app-routes
       (wrap-defaults
-       (-> site-defaults 
+       (-> site-defaults
            (assoc-in [:params :keywordize] true)
            (assoc-in [:security :anti-forgery] false)
            (assoc-in [:session :flash] true)))
